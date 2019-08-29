022871fe5a3347582966d463c77cd2a026a2d22a
Tema Nikitin <megadestroyer95@gmail.com>
2019-08-29 18:22:37 +0300

    заработала навигация

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let wallpaperFullscreen = document.querySelector('.wallpaper--fullscreen')
 buttonBurger.addEventListener('click', function() { wallpaperFullscreen.style.right = '0' })
 let fullscreenExit = document.querySelector('.fullscreen__exit');
 fullscreenExit.addEventListener('click', function() { wallpaperFullscreen.style.right = '-100%' });
+let verticalMenuListLink = document.querySelectorAll('.vertical-menu-list__link');
+for (let i = 0; i < verticalMenuListLink.length; i++) { verticalMenuListLink[i].addEventListener('click', function() { wallpaperFullscreen.style.right = '-100%' }) }
 
 
 let menuAccordeonCard = document.querySelectorAll('.menu-accordeon__card');
