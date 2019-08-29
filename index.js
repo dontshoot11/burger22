ff80974b1acfec47643b76e9aae5553b67533668
Tema Nikitin <megadestroyer95@gmail.com>
2019-08-29 18:05:47 +0300

    немножко оживил аккордеоны

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ let wallpaperFullscreen = document.querySelector('.wallpaper--fullscreen')
 buttonBurger.addEventListener('click', function() { wallpaperFullscreen.style.right = '0' })
 let fullscreenExit = document.querySelector('.fullscreen__exit');
 fullscreenExit.addEventListener('click', function() { wallpaperFullscreen.style.right = '-100%' });
+
+
+let menuAccordeonCard = document.querySelectorAll('.menu-accordeon__card');
+for (let i = 0; i < menuAccordeonCard.length; i++) { menuAccordeonCard[i].addEventListener('click', function() { menuAccordeonCard[i].classList.toggle('menu-accordeon__card--active') }) };
+
+
+let accordeonCard = document.querySelectorAll('.accordeon__card');
+for (let i = 0; i < accordeonCard.length; i++) { accordeonCard[i].addEventListener('click', function() { accordeonCard[i].classList.toggle('accordeon__card--active') }) }
