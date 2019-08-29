78e595d85da46dab54ca74eea9c6f0a04f1eb0eb
Tema Nikitin <megadestroyer95@gmail.com>
2019-08-29 13:32:01 +0300

    первые потуги в js!
    работает полноэкранное меню в мобильной версии!!!

---
+++ index.js
@@ -0,0 +1,5 @@
+let buttonBurger = document.querySelector('.button-burger');
+let wallpaperFullscreen = document.querySelector('.wallpaper--fullscreen')
+buttonBurger.addEventListener('click', function() { wallpaperFullscreen.style.display = 'block' })
+let fullscreenExit = document.querySelector('.fullscreen__exit');
+fullscreenExit.addEventListener('click', function() { wallpaperFullscreen.style.display = 'none' })
