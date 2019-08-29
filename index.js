5c66312380b0c62bc8d6d21ae578e9094f5d49e3
Tema Nikitin <megadestroyer95@gmail.com>
2019-08-29 13:35:10 +0300

    f

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ let buttonBurger = document.querySelector('.button-burger');
 let wallpaperFullscreen = document.querySelector('.wallpaper--fullscreen')
 buttonBurger.addEventListener('click', function() { wallpaperFullscreen.style.display = 'block' })
 let fullscreenExit = document.querySelector('.fullscreen__exit');
-fullscreenExit.addEventListener('click', function() { wallpaperFullscreen.style.display = 'none' })
+fullscreenExit.addEventListener('click', function() { wallpaperFullscreen.style.display = 'none' });
