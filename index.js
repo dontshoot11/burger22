9d7fa3d0b01290a187aac3206d3354c716f6d7ab
Tema Nikitin <megadestroyer95@gmail.com>
2019-08-29 17:18:20 +0300

    добавил анимацию у фуллскрин меню

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let buttonBurger = document.querySelector('.button-burger');
 let wallpaperFullscreen = document.querySelector('.wallpaper--fullscreen')
-buttonBurger.addEventListener('click', function() { wallpaperFullscreen.style.display = 'block' })
+buttonBurger.addEventListener('click', function() { wallpaperFullscreen.style.right = '0' })
 let fullscreenExit = document.querySelector('.fullscreen__exit');
-fullscreenExit.addEventListener('click', function() { wallpaperFullscreen.style.display = 'none' });
+fullscreenExit.addEventListener('click', function() { wallpaperFullscreen.style.right = '-100%' });
