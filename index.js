fade296eeb69072e15c2e984837be9793c9ced64
Tema Nikitin <megadestroyer95@gmail.com>
2019-09-10 15:17:00 +0300

    кнопка ЗАКАЗАТЬ заработала

--- index.js
+++ index.js
@@ -376,6 +376,26 @@ if ($(window).width() > 768) {
 
 
     })
+
+    $('.button--order').on('click',
+        function(e) {
+            wrapper.stop(true, false).animate({
+
+                    'top': -$('.order').index() * 100 + 'vh'
+                }, 300,
+                function() {
+                    let orderIndex = $('.order').index();
+
+                    $('.order').siblings().removeClass('section-active');
+                    $('.order').addClass('section-active');
+                    $('.sidemenu__button:eq(' + orderIndex + ')').addClass('sidemenu__button--active');
+                    $('.sidemenu__button:eq(' + orderIndex + ')').siblings().removeClass('sidemenu__button--active');
+
+                }
+
+
+            )
+        })
 }
 
 
