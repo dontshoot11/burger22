91133fed2057eb374e14b3d8df7edd16ce55f1ac
Tema Nikitin <megadestroyer95@gmail.com>
2019-09-11 04:22:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -504,4 +504,8 @@ function init() {
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
-$(body).ready(function() { wrapper.stop(true, false).animate({ 'top': 10 }, 1), wrapper.stop(true, false).animate({ 'top': 0 }, 1) });
+$(body).ready(function() {
+    wrapper.stop(true, false).animate({ 'top': 100 }, 1, function() {
+        { wrapper.stop(true, false).animate({ 'top': 0 }, 1) }
+    })
+});
